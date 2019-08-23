f67fed328968db565678eddf8c0cefefa949ed9f
virajransing <virajransing@gmail.com>
2019-08-23 17:17:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,15 +12,17 @@ end
 def map_to_no_change(source_array)
   new = []
   i = 0 
-  while i < source_array.lenght do 
+  while i < source_array.length do 
     new.push( source_array[i] )
     i += 1 
   end 
+  return new
+end 
   
 def map_to_double(source_array)
   new = []
   i= 0 
-  while i < source_array.lenght do
+  while i < source_array.length do
     new.push(source_array[i]**2)
     i += 1 
   end 
@@ -30,7 +32,8 @@ end
 def map_to_square(source_array)
   new = []
   i = 0 
-  while i < source_array[i] **2)
+  while i <source_array.length do
+  source_array[i] **2)
   i += 1 
 end 
 return new
